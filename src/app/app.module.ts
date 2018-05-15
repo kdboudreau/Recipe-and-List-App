0c60f04f507244380017f5ebd7137ed2cf8c9097
Kathryn <kdboudreau1@gmail.com>
2018-05-15 16:40:14 -0400

    adding imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HTTPModule }  from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
