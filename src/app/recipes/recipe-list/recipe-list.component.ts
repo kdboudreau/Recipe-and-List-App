38e2b55b31ac7475538b8e28258631e7abadfa7c
Kathryn <kdboudreau1@gmail.com>
2018-05-21 18:15:56 -0400

    html, adding one recipe for display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','this is a test','https://i0.wp.com/www.thecapablestudent.com/wp-content/uploads/2016/10/recipe-575434_640-1.png?fit=640%2C546&ssl=1')
+  ];
   constructor() { }
 
   ngOnInit() {
